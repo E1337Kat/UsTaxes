517ddabac3436bf23d5633c24cc3ec9c748d960f
Ellie Peterson <ekp2h92@gmail.com>
2026-03-18 15:40:05 -0400

    🎉 Add the federal date for 202 fix copy/paste issue seen

---
+++ src/forms/Y2025/data/federal.ts
@@ -0,0 +1,411 @@
+import { FilingStatus } from 'ustaxes/core/data'
+import { linear, Piecewise } from 'ustaxes/core/util'
+
+export const CURRENT_YEAR = 2025
+
+interface TaggedAmount {
+  name: string
+  amount: number
+}
+
+interface Brackets {
+  brackets: number[]
+}
+
+interface Deductions {
+  deductions: TaggedAmount[]
+  exemptions: TaggedAmount[]
+}
+
+interface Rates {
+  rates: number[]
+}
+
+interface FederalBrackets {
+  ordinary: Rates & { status: { [key in FilingStatus]: Brackets & Deductions } }
+  longTermCapGains: Rates & { status: { [key in FilingStatus]: Brackets } }
+}
+
+// Tax brackets can be most easily found via google
+// The standard deduction amounts with the allowances can be most
+// easily found at the end of 1040-SR
+const federalBrackets: FederalBrackets = {
+  ordinary: {
+    rates: [10, 12, 22, 24, 32, 35, 37],
+    status: {
+      [FilingStatus.S]: {
+        brackets: [11925, 48475, 103350, 197300, 250525, 626350],
+        deductions: [
+          {
+            name: 'Standard Deduction (Single)',
+            amount: 15750
+          },
+          {
+            name: 'Standard Deduction (Single) with 1 age or blindness allowance',
+            amount: 17750
+          },
+          {
+            name: 'Standard Deduction (Single) with 2 age or blindness allowances',
+            amount: 19750
+          }
+        ],
+        exemptions: [
+          {
+            name: 'Standard Exemption (Single)',
+            amount: 0
+          }
+        ]
+      },
+      [FilingStatus.MFJ]: {
+        brackets: [23850, 96950, 206700, 394600, 501050, 751600],
+        deductions: [
+          {
+            name: 'Standard Deduction (Married)',
+            amount: 31500
+          },
+          {
+            name: 'Standard Deduction (Married) with 1 age or blindness allowance',
+            amount: 33100
+          },
+          {
+            name: 'Standard Deduction (Married) with 2 age or blindness allowances',
+            amount: 34700
+          },
+          {
+            name: 'Standard Deduction (Married) with 3 age or blindness allowances',
+            amount: 36300
+          },
+          {
+            name: 'Standard Deduction (Married) with 4 age or blindness allowances',
+            amount: 37900
+          }
+        ],
+        exemptions: [
+          {
+            name: 'Standard Exemption (Single)',
+            amount: 0
+          }
+        ]
+      },
+      [FilingStatus.W]: {
+        brackets: [23850, 96950, 206700, 394600, 501050, 751600],
+        deductions: [
+          {
+            name: 'Standard Deduction (Widowed)',
+            amount: 31500
+          },
+          {
+            name: 'Standard Deduction (Widowed) with 1 age or blindness allowance',
+            amount: 33100
+          },
+          {
+            name: 'Standard Deduction (Widowed) with 2 age or blindness allowances',
+            amount: 34700
+          }
+        ],
+        exemptions: [
+          {
+            name: 'Standard Exemption (Widowed)',
+            amount: 0
+          }
+        ]
+      },
+      [FilingStatus.MFS]: {
+        brackets: [11925, 48475, 103350, 197300, 250525, 375800],
+        deductions: [
+          {
+            name: 'Standard Deduction (Married Filing Separately)',
+            amount: 15750
+          },
+          {
+            name: 'Standard Deduction (Married Filing Separately) with 1 age or blindness allowance',
+            amount: 17350
+          },
+          {
+            name: 'Standard Deduction (Married Filing Separately) with 2 age or blindness allowances',
+            amount: 18950
+          },
+          {
+            name: 'Standard Deduction (Married Filing Separately) with 3 age or blindness allowances',
+            amount: 20550
+          },
+          {
+            name: 'Standard Deduction (Married Filing Separately) with 4 age or blindness allowances',
+            amount: 22150
+          }
+        ],
+        exemptions: [
+          {
+            name: 'Standard Exemption (Single)',
+            amount: 0
+          }
+        ]
+      },
+      [FilingStatus.HOH]: {
+        brackets: [17000, 64850, 103350, 197300, 250500, 626350],
+        deductions: [
+          {
+            name: 'Standard Deduction (Head of Household)',
+            amount: 23625
+          },
+          {
+            name: 'Standard Deduction (Head of Household) with 1 age or blindness allowance',
+            amount: 25625
+          },
+          {
+            name: 'Standard Deduction (Head of Household) with 2 age or blindness allowances',
+            amount: 27625
+          }
+        ],
+        exemptions: [
+          {
+            name: 'Standard Exemption (Single)',
+            amount: 0
+          }
+        ]
+      }
+    }
+  },
+  longTermCapGains: {
+    rates: [0, 15, 20],
+    status: {
+      [FilingStatus.S]: {
+        brackets: [48350, 533400]
+      },
+      [FilingStatus.MFJ]: {
+        brackets: [96700, 600050]
+      },
+      [FilingStatus.W]: {
+        brackets: [96700, 600050]
+      },
+      [FilingStatus.MFS]: {
+        brackets: [48350, 300000]
+      },
+      [FilingStatus.HOH]: {
+        brackets: [64750, 566700]
+      }
+    }
+  }
+}
+
+// max tax and income based on https://www.ssa.gov/oact/cola/cbb.html
+// calc the maxIncomeSSTaxApplies with maxSSTax * rate
+// rate is this year 6.2
+export const fica = {
+  maxSSTax: 10918.2,
+  maxIncomeSSTaxApplies: 176100,
+
+  regularMedicareTaxRate: 1.45 / 100,
+  additionalMedicareTaxRate: 0.9 / 100,
+  additionalMedicareTaxThreshold: (filingStatus: FilingStatus): number => {
+    switch (filingStatus) {
+      case FilingStatus.MFJ: {
+        return 250000
+      }
+      case FilingStatus.MFS: {
+        return 125000
+      }
+      default: {
+        return 200000 // Single, Head of Household, Windower
+      }
+    }
+  }
+}
+
+// Net Investment Income Tax calculated on form 8960
+// https://www.irs.gov/instructions/i8960
+export const netInvestmentIncomeTax = {
+  taxRate: 0.038, // 3.8%
+  taxThreshold: (filingStatus: FilingStatus): number => {
+    switch (filingStatus) {
+      case FilingStatus.MFJ: {
+        return 250000
+      }
+      case FilingStatus.W: {
+        return 250000
+      }
+      case FilingStatus.MFS: {
+        return 125000
+      }
+      default: {
+        return 200000 // Single, Head of Household
+      }
+    }
+  }
+}
+
+export const healthSavingsAccounts = {
+  contributionLimit: {
+    'self-only': 4300,
+    family: 8550
+  }
+}
+// https://www.irs.gov/newsroom/irs-provides-tax-inflation-adjustments-for-tax-year-2024
+// https://www.irs.gov/instructions/i6251
+export const amt = {
+  excemption: (
+    filingStatus: FilingStatus,
+    income: number
+  ): number | undefined => {
+    switch (filingStatus) {
+      case FilingStatus.S:
+        if (income <= 626350) {
+          return 88100
+        }
+        break
+      case FilingStatus.MFJ:
+        if (income <= 1252700) {
+          return 137000
+        }
+        break
+      case FilingStatus.MFS:
+        if (income <= 626350) {
+          return 68500
+        }
+    }
+    // TODO: Handle "Exemption Worksheet"
+    return undefined
+  },
+
+  // Used for calculating Line 6 on form 6251 (in 2025). See instructions
+  cap: (filingStatus: FilingStatus): number => {
+    if (filingStatus === FilingStatus.MFS) {
+      return 119550
+    }
+    return 239100
+  }
+}
+
+// https://www.irs.gov/credits-deductions/individuals/earned-income-tax-credit/earned-income-and-earned-income-tax-credit-eitc-tables#EITC%20Tables
+// line 11 caps based on step one in instructions
+const line11Caps = [19104, 50434, 57310, 61555]
+const line11MfjCaps = [26214, 57554, 64430, 68675]
+
+type Point = [number, number]
+
+// Provided a list of points, create a piecewise function
+// that makes linear segments through the list of points.
+const toPieceWise = (points: Point[]): Piecewise =>
+  points
+    .slice(0, points.length - 1)
+    .map((point, idx) => [point, points[idx + 1]])
+    .map(([[x1, y1], [x2, y2]]) => ({
+      // starting point     slope              intercept
+      lowerBound: x1,
+      f: linear((y2 - y1) / (x2 - x1), y1 - (x1 * (y2 - y1)) / (x2 - x1))
+    }))
+
+// These points are taken directly from IRS publication
+// IRS Rev. Proc. 2024-40 for tax year 2025
+// https://www.irs.gov/pub/irs-drop/rp-24-40.pdf
+// tax year 2026: https://www.irs.gov/pub/irs-drop/rp-25-32.pdf
+const unmarriedFormulas: Piecewise[] = (() => {
+  const points: Point[][] = [
+    [
+      [0, 0],
+      [8490, 649],
+      [10620, 4328],
+      [19104, 0]
+    ], // 0
+    [
+      [0, 0],
+      [12730, 4328],
+      [23350, 4328],
+      [50434, 0]
+    ], // 1
+    [
+      [0, 0],
+      [17880, 7152],
+      [23350, 7152],
+      [57310, 0]
+    ], // 2
+    [
+      [0, 0],
+      [17880, 8046],
+      [23350, 8046],
+      [61555, 0]
+    ] // 3 or more
+  ]
+  return points.map((ps: Point[]) => toPieceWise(ps))
+})()
+
+const marriedFormulas: Piecewise[] = (() => {
+  const points: Point[][] = [
+    [
+      [0, 0],
+      [8490, 649],
+      [17730, 4328],
+      [26214, 0]
+    ], // 0
+    [
+      [0, 0],
+      [12730, 4328],
+      [30470, 4328],
+      [57554, 0]
+    ], // 1
+    [
+      [0, 0],
+      [17880, 7152],
+      [30470, 7152],
+      [64430, 0]
+    ], // 2
+    [
+      [0, 0],
+      [17880, 8046],
+      [30470, 8046],
+      [68675, 0]
+    ] // 3 or more
+  ]
+  return points.map((ps) => toPieceWise(ps))
+})()
+
+interface EICDef {
+  caps: { [k in FilingStatus]: number[] | undefined }
+  maxInvestmentIncome: number
+  formulas: { [k in FilingStatus]: Piecewise[] | undefined }
+}
+
+export const QualifyingDependents = {
+  childMaxAge: 17,
+  qualifyingDependentMaxAge: 19,
+  qualifyingStudentMaxAge: 24
+}
+
+export const EIC: EICDef = {
+  // credit caps for number of children (0, 1, 2, 3 or more):
+  // Step 1
+  caps: {
+    [FilingStatus.S]: line11Caps,
+    [FilingStatus.W]: line11Caps,
+    [FilingStatus.HOH]: line11Caps,
+    [FilingStatus.MFS]: undefined,
+    [FilingStatus.MFJ]: line11MfjCaps
+  },
+  maxInvestmentIncome: 11950,
+  formulas: {
+    [FilingStatus.S]: unmarriedFormulas,
+    [FilingStatus.W]: unmarriedFormulas,
+    [FilingStatus.HOH]: unmarriedFormulas,
+    [FilingStatus.MFS]: undefined,
+    [FilingStatus.MFJ]: marriedFormulas
+  }
+}
+
+export default federalBrackets
+
+// Constants used in the social security benefits worksheet
+interface SocialSecurityBenefitsDef {
+  caps: { [k in FilingStatus]: { l8: number; l10: number } }
+}
+
+// TODO: update for Y2025
+// This claims line 8 and line 10, but I don't see where on the SSB worksheet that is the case ever...
+export const SSBenefits: SocialSecurityBenefitsDef = {
+  caps: {
+    [FilingStatus.S]: { l8: 25000, l10: 9000 },
+    [FilingStatus.W]: { l8: 25000, l10: 9000 },
+    [FilingStatus.HOH]: { l8: 25000, l10: 9000 },
+    [FilingStatus.MFS]: { l8: 25000, l10: 9000 },
+    [FilingStatus.MFJ]: { l8: 32000, l10: 12000 }
+  }
+}
